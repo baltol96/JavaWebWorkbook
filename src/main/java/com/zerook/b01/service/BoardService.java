03e729d0667de9b2089f889b5439193f7c7235e8
baltol96 <baltol@naver.com>
2024-07-29 23:41:07 +0900

    게시물 목록 화면 처리

--- src/main/java/com/zerook/b01/service/BoardService.java
+++ src/main/java/com/zerook/b01/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.zerook.b01.service;
 
 import com.zerook.b01.dto.BoardDTO;
+import com.zerook.b01.dto.BoardListReplyCountDTO;
 import com.zerook.b01.dto.PageRequestDTO;
 import com.zerook.b01.dto.PageResponseDTO;
 
@@ -15,5 +16,7 @@ public interface BoardService {
     void remove(Long bno);
 
     PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
+
+    PageResponseDTO<BoardListReplyCountDTO> listWithReplyCount(PageRequestDTO pageRequestDTO);
 
 }
