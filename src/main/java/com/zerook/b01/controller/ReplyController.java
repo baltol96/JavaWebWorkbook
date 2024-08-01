1e1fe6e67847193d80792e9dc513fc0e74ba974c
baltol96 <baltol@naver.com>
2024-08-01 23:57:22 +0900

    댓글 등록 기능 - @RestControllerAdvice를 활용한 에러처리 : 미리 특정 Exception을 등록해 그냥 서버 에러가 아닌 에러 사유를 함께 리턴해 처리한다.

--- src/main/java/com/zerook/b01/controller/ReplyController.java
+++ src/main/java/com/zerook/b01/controller/ReplyController.java
@@ -1,17 +1,18 @@
 package com.zerook.b01.controller;
 
+import com.zerook.b01.dto.PageRequestDTO;
+import com.zerook.b01.dto.PageResponseDTO;
 import com.zerook.b01.dto.ReplyDTO;
+import com.zerook.b01.service.ReplyService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -20,7 +21,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor
 public class ReplyController {
+
+    private final ReplyService replyService;
 
     @Operation(summary = "Replies POST", description = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -34,9 +38,22 @@ public class ReplyController {
         }
 
         Map<String,Long> resultMap = new HashMap<>();
-        resultMap.put("rno", 111L);
+
+        Long rno = replyService.register(replyDTO);
+
+        resultMap.put("rno", rno);
 
         return resultMap;
+
+    }
+
+    @Operation(summary = "Replies of Board", description = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO){
+
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+
+        return responseDTO;
 
     }
 }
