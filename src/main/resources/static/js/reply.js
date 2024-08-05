cfbd12fa8a6655e28385a839641291c78ee2d270
baltol96 <baltol@naver.com>
2024-08-05 23:09:47 +0800

    마지막 페이지로 이동처리

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -2,6 +2,12 @@ async function getList({bno, page, size, goLast}){
 
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
 
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList(({bno:bno, pate:lastPage, size:size}))
+    }
+
     return result.data
 
 }
